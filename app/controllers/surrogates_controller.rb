fee4423a9aefdde50da83b564d429a671ac93e88
Dan Crews <dancrews@me.com>
2014-04-25 13:07:23 -0400

    add edit/update capability for surrogate

--- app/controllers/surrogates_controller.rb
+++ app/controllers/surrogates_controller.rb
@@ -8,4 +8,15 @@ class SurrogatesController < ApplicationController
     @surrogate = Surrogate.find(params[:id])
   end
 
+  def edit
+    @surrogate = Surrogate.find(params[:id])
+  end
+
+  def update
+    @surrogate = Surrogate.find(params[:id])
+    surrogate_params = params.require(:surrogate).permit(:name, :status)
+    @surrogate.update(surrogate_params)
+    redirect_to @surrogate
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "tasks" => "tasks#index"
   get "surrogates" => "surrogates#index"
   get "surrogates/:id" => "surrogates#show", as: "surrogate"
+  get "surrogates/:id/edit" => "surrogates#edit", as: "edit_surrogate"
+  patch "surrogates/:id" => "surrogates#update"
 end
