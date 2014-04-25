90c70c441c9b9ad19b8e25596e9d24da6148d1ee
Dan Crews <dancrews@me.com>
2014-04-25 11:34:26 -0400

    refactor seeds file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root 'surrogates#index'
   get "tasks" => "tasks#index"
   get "surrogates" => "surrogates#index"
   get "surrogates/:id" => "surrogates#show", as: "surrogate"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-    task1 = Task.create(name: 'Write feature spec',
-                        description: 'Write a test that describes what the user should be able to do',
-                        status: 'open')
+Task.create!([
+    {
+    name: 'Write feature spec',
+    description: 'Write a test that describes what the user should be able to do',
+    status: 'open'
+    },
+    {
+    name: 'Write feature spec',
+    description: 'Write a test that describes what the user should be able to do',
+    status: 'closed'
+    },
+    {
+    name: 'Write feature spec',
+    description: 'Write a test that describes what the user should be able to do',
+    status: 'open'
+    }
+])
 
-    task2 = Task.create(name: 'Write feature spec',
-                        description: 'Write a test that describes what the user should be able to do',
-                        status: 'closed')
-
-    task3 = Task.create(name: 'Write feature spec',
-                        description: 'Write a test that describes what the user should be able to do',
-                        status: 'open')
-
-    surrogate1 = Surrogate.create(name: 'Dan',
-                        status: 'applied')
-
-    surrogate2 = Surrogate.create(name: 'Cynthia',
-                        status: 'accepted')
-
-    surrogate3 = Surrogate.create(name: 'Will',
-                        status: 'approved')
+Surrogate.create!([
+    {
+    name: 'Dan',
+    status: 'applied'
+    },
+    {
+    name: 'Cynthia',
+    status: 'accepted'
+    },
+    {
+    name: 'Will',
+    status: 'approved'
+    }
+])
